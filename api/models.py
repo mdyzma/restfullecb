b14c8f651b8672bc7d10639fe28a48d0028bcf10
Michal Dyzma <mdyzma@gmail.com>
2018-07-04 07:08:30 +0200

    Added and migrated currency model

--- api/models.py
+++ api/models.py
@@ -1,4 +1,11 @@
 from django.db import models
 
 class Currency(models.Model):
-    pass
+    """Class represents currency record stored in DB"""
+    date_created = models.DateTimeField(auto_now=True)
+    symbol = models.CharField(max_length=3, blank=False, unique=True)
+    ecb_rate = models.FloatField(default=1.0)
+
+    def __str__(self):
+        """Human readible representation of the record"""
+        return "{}".format()
